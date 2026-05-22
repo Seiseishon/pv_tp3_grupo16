4a98ce0fa668a91c8aa85d133a2cdcf69cda394e
Nelson-Ramos07 <fr0788485@gmail.com>
2026-05-22 15:45:07 -0300

    Se agrego el uso de los hooks

--- src/components/ListaProyectos.jsx
+++ src/components/ListaProyectos.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
 import proyectoService from '../services/proyectoService';
 
 // Le pasamos una "orden" (función) llamada alSeleccionarProyecto que viene desde App.jsx
 const ListaProyectos = ({ alSeleccionarProyecto }) => {
-  const proyectos = proyectoService.obtenerProyectos();
+  
+  const [proyectos, setProyectos] = useState(proyectoService.obtenerProyectos());
+  const [busqueda, setBusqueda] = useState('');
+
+  const eliminarProyecto = (id) => {
+    proyectoService.eliminarProyecto(id);
+    setProyectos(proyectoService.obtenerProyectos());
+    // Aquí podrías agregar lógica para actualizar la lista de proyectos después de eliminar uno
+  }
+  
+  const buscarProyectos = () => {
+    if (busqueda.trim() === '') {
+      return proyectos; // Si no hay búsqueda, mostrar todos los proyectos
+    } else {
+      return proyectos.filter(proyecto => 
+        proyecto.titulo.toLowerCase().includes(busqueda.toLowerCase())
+      );
+    }
+  };
+
+  const proyectosFiltrados = buscarProyectos(); 
 
   return (
     <main>
@@ -12,11 +33,13 @@ const ListaProyectos = ({ alSeleccionarProyecto }) => {
         <input 
           type="text" 
           placeholder="Buscar proyecto por título..." 
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
         />
       </div>
 
       <div className="contenedor-proyectos">
-        {proyectos.map((proyecto) => (
+        {proyectosFiltrados.map((proyecto) => (
           <div className="card-proyecto" key={proyecto.id}>
             <h3>{proyecto.titulo}</h3>
             <p><strong>Categoría:</strong> {proyecto.categoria}</p>
@@ -30,7 +53,10 @@ const ListaProyectos = ({ alSeleccionarProyecto }) => {
               Ver Detalle
             </button>
 
-            <button style={{ backgroundColor: '#ff4757', color: 'white' }}>
+            <button 
+              onClick={() => eliminarProyecto(proyecto.id)}
+              style={{ backgroundColor: '#ff4757', color: 'white' }}
+            >
               Eliminar
             </button>
           </div>
