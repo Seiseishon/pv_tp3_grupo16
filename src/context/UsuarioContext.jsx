5c01d29462db6fbda12dd8ae8f4176204bdc393d
SergiosSozas1621 <46791457@fi.unju.edu.ar>
2026-06-17 02:07:10 -0300

    Se agrego la Persistecia del Estado Global

--- src/context/UsuarioContext.jsx
+++ src/context/UsuarioContext.jsx
@@ -1,18 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const UsuarioContext = createContext();
 
 export const UsuarioProvider = ({ children }) => {
-    const [usuario, setUsuario] = useState({
-        nombre: "Gerardo",
-        dni: "45672154",
-        rol: "Alumno",
-        institucion: "Universidad Nacional de Jujuy"
-    });
+    
+    const usuarioGuardado = localStorage.getItem("usuario_sesion");
+    
+    const estadoInicial = usuarioGuardado 
+        ? JSON.parse(usuarioGuardado) 
+        : {
+            nombre: "Gerardo",
+            dni: "45672154",
+            rol: "Alumno",
+            institucion: "Universidad Nacional de Jujuy"
+          };
+
+    const [usuario, setUsuario] = useState(estadoInicial);
 
     const actualizarPerfil = (nuevosDatos) => {
         setUsuario((prev) => ({ ...prev, ...nuevosDatos }));
     };
+
+    useEffect(() => {
+        localStorage.setItem("usuario_sesion", JSON.stringify(usuario));
+    }, [usuario]); 
 
     return (
         <UsuarioContext.Provider value={{ usuario, actualizarPerfil }}>
